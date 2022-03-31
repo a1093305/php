43ab7d9e3026de491531c689648c20a317d87539
a1093305 <100325724+a1093305@users.noreply.github.com>
2022-03-31 12:05:55 +0800

    Delete 0331_作業 directory

--- 0331_作業/conn.php
+++
@@ -1,9 +0,0 @@
-<?php
-    $hostname = 'localhost';
-    $username = 'trip';
-    $password = 'trip';
-    $dbname = 'php_hw_users';
-
-    $conn = mysqli_connect($hostname,$username, $password, $dbname);
-    $conn -> set_charset("UTF8");
-?>
--- 0331_作業/index.php
+++
@@ -1,9 +0,0 @@
-<?php
-    header('location:login.php');
-?>
-
-<html>
-    <head>
-        <title>正在重導向中……</title>
-    </head>
-</html>
